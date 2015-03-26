270b70f4bcc0f540e6cf89a527984f6217d87d6d
Beatrice7 <aizun777@163.com>
2015-03-26 22:05:18 +0800

    31 nextPermutation 2.0

--- Permutation/31_NextPermutation.cpp
+++ Permutation/31_NextPermutation.cpp
@@ -14,24 +14,23 @@ Here are some examples. Inputs are in the left-hand column and its corresponding
 
 class Solution {
 public:
-    void nextPermutation(vector<int> &num) {
+    void nextPermutation(vector<int> &num){
         int n = num.size();
         if(n<=1) return;
-        for(int index = n-1; index > 0;){
-            while(num[index] <= num[index-1] && index >= 1) --index; 
-            if(index == 0){
-                reverse(num.begin(), num.end());
+        int index = n-1;
+        while(num[index] <= num[index-1] && index >= 1) --index; 
+        if(index == 0){
+            reverse(num.begin(), num.end());
+            return;
+        }
+        /*从后面的数组(一定是升序了)中找出比num[index-1]大的数，而不是直接与num[index]交换！*/
+    	for(int i = n-1; i >= index; --i){
+            if(num[i] > num[index-1]) {
+                int tmp = num[i];
+                num[i] = num[index-1];
+                num[index-1] = tmp;
+                reverse(num.begin() + index, num.end());
                 return;
-            }
-            /*从后面的数组(一定是升序了)中找出比num[index-1]大的数，而不是直接与num[index]交换！*/
-            for(int i = n-1; i >= index; --i){
-                if(num[i] > num[index-1]) {
-                    int tmp = num[i];
-                    num[i] = num[index-1];
-                    num[index-1] = tmp;
-                    reverse(num.begin() + index, num.end());
-                    return;
-                }
             }
         }
        // for(auto it = num.begin(); it < num.end(); it++) cout << *it << " ";
