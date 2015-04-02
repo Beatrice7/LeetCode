5ce22ff92a3dcf8b30e4710169c0949de3dc2286
Beatrice7 <aizun777@163.com>
2015-04-02 21:39:32 +0800

    solution2

--- SetMatrixZeroes/73_SetMatrixZeroes.cpp
+++ SetMatrixZeroes/73_SetMatrixZeroes.cpp
@@ -14,7 +14,7 @@
 *               
 **********************************************************************************/
 
-
+//O(m + n) space
 class Solution {
 public:
     void setZeroes(vector<vector<int> > &matrix) {
@@ -46,5 +46,51 @@ public:
                 }
             }
         }
+    }
+};
+
+//a constant space
+class Solution {
+public:
+    void setZeroes(vector<vector<int> > &matrix) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int firstrow = false;
+        int firstcol = false;
+        
+        for(int i = 0; i < row; ++i){
+            for(int j = 0; j < col; ++j){
+                if(matrix[i][j] == 0){
+                    if(i == 0) firstrow = true;
+                    if(j == 0) firstcol = true;
+                    matrix[i][0] = matrix[0][j] = 0;
+                   
+                }
+            }
+        }
+        
+        for(int i = 1; i < row; ++i){
+            if(matrix[i][0] == 0){
+                fill(&matrix[i][0], &matrix[i][0] + col, 0);
+            }
+        }
+        
+        for(int j = 1; j < col; ++j){
+            if(matrix[0][j] == 0){
+                for(int k = 0; k < row; ++k){
+                    matrix[k][j] = 0;
+                }        
+            }
+        }
+        
+        if(firstrow){
+            fill(&matrix[0][0], &matrix[0][0] + col, 0);
+        }
+        if(firstcol){
+            for(int k = 0; k < row; ++k){
+                    matrix[k][0] = 0;
+                }         
+        }
+        
     }
 };
