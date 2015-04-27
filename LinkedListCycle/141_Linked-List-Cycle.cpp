d30957a2ec7967ec53ef105ef717f59b6ab67e11
Beatrice7 <aizun777@163.com>
2015-04-27 23:36:59 +0800

    141 fast pointer and slow pointer

---
+++ LinkedListCycle/141_Linked-List-Cycle.cpp
@@ -0,0 +1,41 @@
+/********************************************************************************** 
+* 
+* Given a linked list, determine if it has a cycle in it.
+* 
+* Follow up:
+* Can you solve it without using extra space?
+* 
+*               
+**********************************************************************************/
+
+/*
+ * if there is a cycle in the list, then we can use two pointers travers the list.
+ *
+ * one pointer traverse one step each time, another one traverse two steps each time.
+ *
+ * so, those two pointers meet together, that means there must be a cycle inside the list.
+ */
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *fast, *slow;
+        fast = slow = head;
+        
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast) return true;
+        }
+        return false;
+    }
+};
